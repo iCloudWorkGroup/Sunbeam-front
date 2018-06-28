c70df32a2bfb927642ac12b9381add2bd1bfd25e
CJL5678 <2737812732@qq.com>
2018-06-28 19:59:18 +0800

    修改代码格式

--- src/components/toolbar/clip.vue
+++ src/components/toolbar/clip.vue
@@ -1,18 +1,18 @@
 <template>
 <div class="fui-body">
-        <span class="fui-layout">
-            <div class="fui-section fui-alone" @click="paste">
-                <div class="fui-cf-ico ico-paste"></div><div class="fui-cf-desc"><div class="fui-cf-text">粘贴</div></div>
-            </div>
-        </span>
-        <span class="fui-layout">
-            <div class="fui-section fui-transverse" @click="cut">
-                <div class="fui-transverse-model"><span class="fui-cf-ico ico-section-ico ico-shear"></span><span class="fui-cf-text">剪切</span></div>
-            </div>
-            <div class="fui-section fui-transverse" @click="copy">
-                <div class="fui-transverse-model"><span class="fui-cf-ico ico-section-ico ico-copy"></span><span class="fui-cf-text">复制</span></div>
-            </div>
-        </span>
+    <span class="fui-layout">
+        <div class="fui-section fui-alone" @click="paste">
+            <div class="fui-cf-ico ico-paste"></div><div class="fui-cf-desc"><div class="fui-cf-text">粘贴</div></div>
+        </div>
+    </span>
+    <span class="fui-layout">
+        <div class="fui-section fui-transverse" @click="cut">
+            <div class="fui-transverse-model"><span class="fui-cf-ico ico-section-ico ico-shear"></span><span class="fui-cf-text">剪切</span></div>
+        </div>
+        <div class="fui-section fui-transverse" @click="copy">
+            <div class="fui-transverse-model"><span class="fui-cf-ico ico-section-ico ico-copy"></span><span class="fui-cf-text">复制</span></div>
+        </div>
+    </span>
 </div>
 
 </template>
